c681dfa3879f748fa6f6c67b5c2d15336b9dbb03
G184562021 <218456@st.takushoku-u.ac.jp>
2022-07-07 15:23:31 +0900

    7月7日

--- kazuate.js
+++ kazuate.js
@@ -7,54 +7,57 @@ let kaisu = 0;
 
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
-hantei();
-hantei();
-hantei();
-hantei();
+
+    let b1 = document.querySelector('button#blue');
+    b1.addEventListener('click', hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-    let yoso = 1;       // 第5回課題:テキストボックスの数値をここに代入
+    // 第5回課題:テキストボックスの数値をここに代入
+    
+    
+    kaisu = kaisu+1;
+    let i = document.querySelector('input[name="yoso"]');
+    yoso = Number(i.value);
+    
+
+    let li = document.querySelector('span#answer');
+    //a.textContent = kaisu;
+    
+    
+    //return;
+
+
+    
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
 
-    for(let n of kaisu){
-        if(kaisu=>4){
-            let answer='回目の予想';
-            result=答えは4でした.すでにゲームは終わっています
-            let s = document.querySelector('p#result');
-s.textContent = result;
-        }
-        else if(kaisu==3 && yoso!=kotae){
-            console.log('３回目の予想');
-            result=まちがい.残念でした答えは4です
-            let result = document.querySelector('p#result');
-s.textContent = result;
-        }
-        else if(kaisu<1 && yoso!=kotae){
-            if(yoso<kotae){
-                console.log(kaisu+'回目の予想');
-                result=まちがい.答えはもっと大きいですよ
-                let result = document.querySelector('p#result');
-s.textContent = result;
-            }
-            if(yoso>kotae){
-                console.log(kaisu+'回目の予想');
-                result=まちがい.答えはもっと小さいですよ
-                let result = document.querySelector('p#result');
-s.textContent = result;
+        
+    
+        if(kaisu>4){
+            li.textContent="答えは"+kotae+"でした。すでにゲームは終わっています。";
+        }else{
+            if(4==kaisu && !(kotae===yoso)){
+                li.textContent="まちがい。残念でした答えは"+kotae+"です";
+            }else if(kaisu<4 && kotae === yoso){
+                li.textContent="正解です、おめでとう";
+                kaisu =kaisu+4;
+            }else if(kaisu<4 && !(kotae === yoso)){
+                if(kotae>yoso){
+                    li.textContent="まちがい。答えはもっと大きいですよ";
+                }else if(kaisu<yoso){
+                    li.textContent="まちがい。答えはもっと小さいですよ";
+                }
             }
         }
-        if(kaisu>=3 && yoso==kotae){
-            result=正解です.おめでとう
-            let result = document.querySelector('p#result');
-s.textContent = result;
-        }
+
+        
+
         let s = document.querySelector('span#kaisu');
         s.textContent = kaisu;
         
         let t = document.querySelector('p#result');
-        s.textContent = kaisu;
+        s.textContent = li;
     }
-}
+
