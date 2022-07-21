b1578464ae3c7b804abab033a9767284e939e6b8
G184562021 <218456@st.takushoku-u.ac.jp>
2022-07-21 15:25:59 +0900

    7月21日

---
+++ practice-ajax/ajax-demo.js
@@ -0,0 +1,51 @@
+//let b = document.querySelector('#sendRequest');
+//b.addEventListener('click', sendRequest);
+
+
+// 通信を開始する処理
+function sendRequest() {
+	// 検索キーを取得する
+	service = document.querySelector('#print');
+    service.addEventListener('click', axios.get);
+	genre = document.querySelector('#print');
+    genre.addEventListener('click', axios.get);
+	// URL を設定
+	//let url = 'https://www.nishita-lab.org/web-contents/jsons/test.json';
+	let url ='https://www.nishita-lab.org/web-contents/jsons/nhk/'+service+'-'+genre+'-j.json'
+
+	// 通信開始
+	//axios.get(通信先)
+	axios.get(url)
+		.then(showResult)/*通信できた時の処理*/ 
+		.catch(showError)/*できなかった時の処理*/
+		.then(finish);/*最後にする処理*/
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+	// サーバから送られてきたデータを出力
+	let data = resp.data;
+
+	// data が文字列型なら，オブジェクトに変換する
+	if (typeof data === 'string') {
+		data = JSON.parse(data);
+	}
+
+	
+	/*検索結果を表示させる*/
+	// data をコンソールに出力
+	console.log(data);
+
+	// data.x を出力
+	console.log(data.x);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+	console.log(err);
+}	
+
+// 通信の最後にいつも実行する処理
+function finish() {
+	console.log('Ajax 通信が終わりました');
+}
--- tv-program.js
+++ tv-program.js
@@ -1,84 +1,59 @@
-let data = {
-  "list": {
-    "g1": [
-      {
-        "id": "2022030428673",
-        "event_id": "28673",
-        "start_time": "2022-03-04T04:35:00+09:00",
-        "end_time": "2022-03-04T04:40:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "みんなのうた「ごっつぉさま」／「超変身！ミネラルフォーマーズ」",
-        "subtitle": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "content": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "act": "",
-        "genres": [
-          "0409",
-          "0700",
-          "0504"
-        ]
-      },
-      {
-        "id": "2022030427069",
-        "event_id": "27069",
-        "start_time": "2022-03-04T23:05:00+09:00",
-        "end_time": "2022-03-04T23:10:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "パラスポーツ×アニメ「アニ×パラ」▽パラアルペンスキーテーマ曲江口寿史×ＡＣＣ",
-        "subtitle": "パラスポーツの魅力をアニメで伝える番組。高速滑走に挑む精神力が試されるパラアルペンスキーを描く。キャラ原案：江口寿史／曲：Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ",
-        "content": "パラスポーツの魅力をアニメで伝えるプロジェクトの第１３弾。圧倒的なスピードに挑む「パラアルペンスキー」の世界を江口寿史原案の魅力的なキャラクターで描く。平昌パラリンピック金メダリストの村岡桃佳選手への取材から生まれた主人公・桃は、スピードへの恐怖を克服していく。その壁を越えた先にあるものとは…　テーマ曲　♪「Ｏｎ　Ｙｏｕｒ　Ｍａｒｋ」はＡｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂが手掛けた。",
-        "act": "【声】松本まりか，【出演】Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ，【監督】西村一彦，【脚本】加納新太，【原案】江口寿史",
-        "genres": [
-          "0700"
-        ]
-      }
-    ]
-  }
-};
-
 /////////////////// 課題3-2 はここから書き始めよう
-console.log("テレビの番組表："+data.list.g1[0].title);
-console.log("テレビの番組表："+data.list.g1[1].title);
+//console.log("テレビの番組表："+data.list.g1[0].title);
+//console.log("テレビの番組表："+data.list.g1[1].title);
+
+//let b = document.querySelector('#sendRequest');
+//b.addEventListener('click', sendRequest);
+//
+//
+//
+//
+
+service = document.querySelector('#sendRequest');
+service.addEventListener('click',sendRequest );
+genre = document.querySelector('#sendRequest');
+genre.addEventListener('click',sendRequest );
+
+// 通信を開始する処理
+function sendRequest() {
+	// 検索キーを取得する
+
+	// URL を設定
+	//let url = 'https://www.nishita-lab.org/web-contents/jsons/test.json';
+	let url ='https://www.nishita-lab.org/web-contents/jsons/nhk/'+service+'-'+genre+'-j.json'
+  let li = document.querySelector('span#service');
+
+  // 通信開始
+	axios.get(url)
+  .then(showResult)
+  .catch(showError)
+  .then(finish);
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+
+// サーバから送られてきたデータを出力
+let data = resp.data;
+
+// data が文字列型なら，オブジェクトに変換する
+if (typeof data === 'string') {
+  data = JSON.parse(data);
+}
+
+// data をコンソールに出力
+console.log(data);
+
+// data.x を出力
+console.log(data.x);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+console.log(err);
+}	
+
+// 通信の最後にいつも実行する処理
+function finish() {
+console.log('Ajax 通信が終わりました');
+}
